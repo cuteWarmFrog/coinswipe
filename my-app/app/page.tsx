8b3dd4634e1ad6e70f16281b7d7d1ac5d7b79e76
v.livshits <v.livshits@sdventures.com>
2024-09-07 12:15:20 +0200

    init boys

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            LET"S FUCKING GO BOYS!!!!!
+            LET&apos;S FUCKING GO BOYS!!!!!
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>

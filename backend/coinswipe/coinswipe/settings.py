13affa134b3c93a699026175878ef46ab7aa1299
Dymirt <kolidadmytro@gmail.com>
2024-09-07 16:01:47 +0200

    adding alowed hosts
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- backend/coinswipe/coinswipe/settings.py
+++ backend/coinswipe/coinswipe/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coinswipe.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
